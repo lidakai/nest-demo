99aa7efccf26997e29e0a89a1a2134aa810b38eb
nian_yi <nianyi@dtstack.com>
2022-07-09 17:16:18 +0800

    feat: list token

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,18 +8,20 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('add')
-  async addUser(@Param() user: UserAddDto, @Headers('token') header?: string) {
-    return this.userService.addUser(Object.assign(user, { token: header }));
+  async addUser(@Param() user: UserAddDto, @Headers('token') token?: string) {
+    return this.userService.addUser(Object.assign(user, { token }));
   }
 
   @Get('list')
   async listUsers(
     @Param('pageNo') pageNo = 1,
     @Param('pageSize') pageSize = 20,
+    @Headers('token') token?: string,
   ) {
     const _pageNo = typeof pageNo === 'string' ? parseInt(pageNo) : pageNo;
     const _pageSize =
       typeof pageSize === 'string' ? parseInt(pageSize) : pageSize;
+    console.log(token, 'list - token');
     return this.userService.listUsers(_pageNo, _pageSize);
   }
 }
